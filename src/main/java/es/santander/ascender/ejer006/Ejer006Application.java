50dca192bf2e5f41f71a3e15474bec1296e4d997
Alejandro Caldi <alejandro.caldi@gmail.com>
2025-02-25 11:23:18 +0100

    Primer commit de instalación

---
+++ src/main/java/es/santander/ascender/ejer006/Ejer006Application.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.ejer006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejer006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejer006Application.class, args);
+	}
+
+}
